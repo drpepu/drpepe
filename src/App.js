c910112758e2add0fe9a5e6b01bc6ab12c4a495f
Victor Maragioglio <maragiogliov@protonmail.com>
2024-08-27 16:04:52 +0100

    video added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
+import VideoComparison from './components/VideoComparison/VideoComparison';
 
 function App() {
   return (
     <>
     <Hero />
+    <VideoComparison />
+
     </>
   );
 }
---
+++ src/components/VideoComparison/VideoComparison.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../VideoComparison/VideoComparison.module.css';
+
+function VideoComparison() {
+  return (
+    <>
+      <div className={styles.videoComparison_container}>
+        <div className={styles.videoComparison_main_text}>
+          THE FIRST DECENTRALIZED SMART AGENT SPECIFICALLY DESIGNED TO HELP YOU UNDERSTAND HOW TO LIVE LONGER, LEVERAGING AN ELITE PEER-REVIEWED RESEARCH DATABASE
+        </div>
+
+        <div className={styles.videoComparison_videos_container}>
+
+            <div className={styles.videoComparison_video_one_container}>
+                <div className={styles.videoComparison_title_one}>Woke GPT</div>
+                <div className={styles.videoComparison_video_one}>
+                    <video autoplay="autoplay" muted="muted" controls="controls" loop="loop"  width="100%" >
+                        <source src="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4 " type="video/mp4"  />
+                    </video>
+                </div>
+            </div>
+
+
+            <div className={styles.videoComparison_video_two_container}>
+                <div className={styles.videoComparison_title_two}>DR. PEPE</div>
+                    <div className={styles.videoComparison_video_two}>
+                    <video autoplay="autoplay" muted="muted" controls="controls" loop="loop"  width="100%"  >
+                        <source src="https://storage.googleapis.com/gtv-videos-bucket/sample/ForBiggerMeltdowns.mp4" type="video/mp4"  />
+                    </video>
+                </div>
+            </div>
+
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default VideoComparison;
