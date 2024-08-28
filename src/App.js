0c17ee156b13ee80df945f89d807d17ee1b039af
Victor Maragioglio <maragiogliov@protonmail.com>
2024-08-28 18:58:58 +0100

    token and footer

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
 import Hero from './components/Hero/Hero';
 import VideoComparison from './components/VideoComparison/VideoComparison';
-import Reasons from './components/Reasons/Reasons'
+import Reasons from './components/Reasons/Reasons';
+import Tokenomics from './components/Tokenomics/Tokenomics';
+import Footer from './components/Footer/Footer';
 
 function App() {
   return (
@@ -9,6 +11,8 @@ function App() {
     <Hero />
     <VideoComparison />
     <Reasons />
+    <Tokenomics />
+    <Footer />
 
 
     </>
---
+++ src/components/Tokenomics/Tokenomics.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styles from './Tokenomics.module.css'
+
+function Tokenomics() {
+  return (
+    <div className={styles.tokenomics_container}>
+        <div className={styles.tokenomics_button_one}></div>
+        <div className={styles.tokenomics_button_two}></div>
+        <div className={styles.tokenomics_button_three}></div>
+    </div>
+  )
+}
+
+export default Tokenomics
