6a3d042bba1d4423c3292097324b878fa35886bb
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-03 11:51:05 +0100

    footer go

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,6 +5,28 @@ function Footer() {
   return (
     <div className={styles.footer_container}>
 
+
+      <div className={styles.footer_newsletter_container}>
+            <div className={styles.footer_newsletter_text_container}>
+                <div>STAY IN THE LOOP WITH</div>
+                <div>DR. PEPE NEWSLETTER</div>
+            </div>
+
+            <div className={styles.footer_newsletter_form_container}>
+                  <div className={styles.footer_newsletter_form}>Enter your email address</div>
+                  <div className={styles.footer_newsletter_form_button}>SUBSCRIBE</div>
+            </div>
+      </div>
+      
+
+
+
+      <div className={styles.footer_socials_container}>
+          <div className={styles.footer_social_button}>X</div>
+          <div className={styles.footer_social_button}>TELEGRAM</div>
+      </div>
+
+
     </div>
   )
 }
