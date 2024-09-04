d1166318abef4887ddc43c23f26cf06326b87ab8
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-04 23:40:31 +0100

    go

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,10 +17,14 @@ function Footer() {
                   <div className={styles.footer_newsletter_form_button}>SUBSCRIBE</div>
             </div>
       </div>
+
+      <div className={styles.footer_buttons_container}>
+          <div className={`${styles.footer_button_one } ${styles.footer_button}`}>DOCS</div>
+          <div className={`${styles.footer_button_two } ${styles.footer_button}`}>APP</div>
+          <div className={`${styles.footer_button_three } ${styles.footer_button}`}>DRPEPEAI</div>
+          <div className={`${styles.footer_button_four } ${styles.footer_button}`}>EN</div>
+      </div>
       
-
-
-
       <div className={styles.footer_socials_container}>
           <div className={styles.footer_social_button}>X</div>
           <div className={styles.footer_social_button}>TELEGRAM</div>
