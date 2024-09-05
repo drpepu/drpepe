d0b31973ea08d6429031526e2ebde50f9b3519c5
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-05 16:22:14 +0100

    url fix

--- src/newsletter_service/getResponseService.js
+++ src/newsletter_service/getResponseService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://getresponse-proxy-server-drpepe.vercel.app/'; 
+const API_URL = 'https://getresponse-proxy-server-drpepe.vercel.app/add-subscriber'; 
 
 export const addSubscriber = async (listId, email, name) => {
   try {
