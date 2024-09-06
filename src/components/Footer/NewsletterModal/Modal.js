729214ceedf391cf3f9017537a1eb33980d77e2b
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-06 15:50:36 +0100

    Modal gsap

--- src/components/Footer/NewsletterModal/Modal.js
+++ src/components/Footer/NewsletterModal/Modal.js
@@ -1,14 +1,53 @@
 // src/components/Modal.js
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import gsap from 'gsap';
 import styles from './Modal.module.css';
-import pepemodal from '../../../Assets/DRPEPEFAT.svg'
+import pepemodal from '../../../Assets/DRPEPEFAT.svg';
 
 const Modal = ({ message, onClose }) => {
+  const modalRef = useRef(null);
+
+  useEffect(() => {
+
+    gsap.fromTo(
+      modalRef.current,
+      { scale: 0.8, opacity: 0 },
+      { 
+        scale: 1, 
+        opacity: 1, 
+        duration: 0.8, 
+        ease: 'elastic.out(1, 0.5)' 
+      }
+    );
+  }, []);
+
+
+  const handleClose = () => {
+
+    gsap.to(modalRef.current, {
+      scale: 0.8, 
+      opacity: 0,
+      duration: 0.5,
+      ease: 'power2.inOut', 
+      onComplete: onClose, 
+    });
+  };
+
   return (
-    <div className={styles.modal_overlay} onClick={onClose}>
-      <div className={styles.modal_content} onClick={(e) => e.stopPropagation()}>
-        <span className={styles.modal_close} onClick={onClose}>&times;</span>
-        <img src={pepemodal}alt='PEPE SUCCES' className={styles.modal_image}></img>
+    <div className={styles.modal_overlay} onClick={handleClose}>
+      <div
+        className={styles.modal_content}
+        onClick={(e) => e.stopPropagation()}
+        ref={modalRef}
+      >
+        <span className={styles.modal_close} onClick={handleClose}>
+          &times;
+        </span>
+        <img
+          src={pepemodal}
+          alt='PEPE SUCCESS'
+          className={styles.modal_image}
+        />
         <p className={styles.modal_text}>{message}</p>
       </div>
     </div>
