5442893fa09a17a6e2eed5235b0a575a393f91cc
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-09 16:50:51 +0100

    go

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 import styles from './Hero.module.css';
 import Header from '../Header/Header';
 import DrPepeVaccine from '../../Assets/DRPEPEVACCINE.svg';
@@ -7,50 +7,53 @@ import solanapill from '../../Assets/SOLANAPILL.svg';
 
 import { useTranslation } from 'react-i18next';
 
-
-
-
 function Hero() {
   const { t } = useTranslation();
 
+
+  const scrollToEnd = () => {
+    window.scrollTo({ top: document.body.scrollHeight, behavior: 'smooth' });
+  };
+
+
+  const goToTelegram = () => {
+    window.open('https://t.me/+yxh5qd2tKQU2ODIx', '_blank');
+  };
+
   return (
     <>
+      <Header />
 
-    <Header />
-
-    <div className={styles.hero_main_text_container}>
-        <img src={DrPepeVaccine}alt='Dr Pepe' className={styles.hero_drpepe_image_one}></img>
+      <div className={styles.hero_main_text_container}>
+        <img src={DrPepeVaccine} alt='Dr Pepe' className={styles.hero_drpepe_image_one} />
         <div className={styles.hero_main_text_one_container}>
-            <div className={styles.hero_main_text_one} >DR. PEPE’S</div>
+          <div className={styles.hero_main_text_one}>DR. PEPE’S</div>
         </div>
         <div className={styles.hero_main_text_two}>LONGEVITY</div>
 
-        <div className={styles.hero_main_text_three_container} >
+        <div className={styles.hero_main_text_three_container}>
           <div className={styles.hero_main_text_three}>LAB </div>
-
-      
-          <img src={solanapill} alt=''className={styles.hero_solana_logo}></img>
-      
-
+          <img src={solanapill} alt='' className={styles.hero_solana_logo} />
         </div>
+      </div>
 
-    </div>
-
-    <div className={styles.hero_secondary_text_container}>
+      <div className={styles.hero_secondary_text_container}>
         <div className={styles.hero_secondary_text_one}>{t('hero_subtitle_line_one')}</div>
         <div className={styles.hero_secondary_text_two}>{t('hero_subtitle_line_two')}</div>
 
         <div className={styles.hero_buttons_container}>
-          <div className={styles.hero_button_one}>{t('hero_button_one')}</div>
-          <div className={styles.hero_button_two}>{t('hero_button_two')}</div>
+          <button className={styles.hero_button_one}  onClick={goToTelegram}>
+            {t('hero_button_one')}
+          </button>
+          <button className={styles.hero_button_two} onClick={scrollToEnd}>
+            {t('hero_button_two')}
+          </button>
         </div>
 
-
-        <img src={DrPepeFat}alt='Dr Pepe' className={styles.hero_drpepe_image_two}></img>
-    </div>
-
+        <img src={DrPepeFat} alt='Dr Pepe' className={styles.hero_drpepe_image_two} />
+      </div>
     </>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
