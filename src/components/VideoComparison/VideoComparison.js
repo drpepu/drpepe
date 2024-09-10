cd1a5d7d202eb92aacfae86c05995cd9f689dca5
Victor Maragioglio <maragiogliov@protonmail.com>
2024-09-10 10:01:33 +0100

    go

--- src/components/VideoComparison/VideoComparison.js
+++ src/components/VideoComparison/VideoComparison.js
@@ -25,10 +25,9 @@ function VideoComparison() {
                 <div className={styles.videoComparison_title_one}>Woke GPT</div>
                 <div className={styles.videoComparison_video_one}>
                   <iframe
-                      // slow src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/e991f0106544d8051a5888c8e52e8777/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2Fe991f0106544d8051a5888c8e52e8777%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
+                      src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/e991f0106544d8051a5888c8e52e8777/iframe?muted=true&preload=true&loop=true&autoplay=false&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2Fe991f0106544d8051a5888c8e52e8777%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
 
-                      src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/168b833791bfcfbb0c56da5ab74dbd7a/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2F168b833791bfcfbb0c56da5ab74dbd7a%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
-
+                
 
 
 
@@ -47,9 +46,9 @@ function VideoComparison() {
                   <div className={styles.videoComparison_title_two}>DR. PEPE</div>
                       <div className={styles.videoComparison_video_two}>
                       <iframe
-                        // slow src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/64c32ee0571ff0d50fe145b222f57b04/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2F64c32ee0571ff0d50fe145b222f57b04%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
+                        src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/64c32ee0571ff0d50fe145b222f57b04/iframe?muted=true&preload=true&loop=true&autoplay=false&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2F64c32ee0571ff0d50fe145b222f57b04%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600&controls=false"
 
-                        src="https://customer-jjq55o3dxtfayxo5.cloudflarestream.com/4c0186c26358508f32924be10622187b/iframe?muted=true&preload=true&loop=true&autoplay=true&poster=https%3A%2F%2Fcustomer-jjq55o3dxtfayxo5.cloudflarestream.com%2F4c0186c26358508f32924be10622187b%2Fthumbnails%2Fthumbnail.jpg%3Ftime%3D%26height%3D600"
+                   
 
 
 
