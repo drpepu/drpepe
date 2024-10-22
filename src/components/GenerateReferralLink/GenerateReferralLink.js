5720df8eeddf4e22d4abb6e65c97a8353f37e4ca
Victor Maragioglio <maragiogliov@protonmail.com>
2024-10-23 00:52:16 +0100

    UI Around

--- src/components/GenerateReferralLink/GenerateReferralLink.js
+++ src/components/GenerateReferralLink/GenerateReferralLink.js
@@ -4,7 +4,6 @@ import styles from './GenerateReferralLink.module.css';
 
 const GenerateReferralLink = () => {
   const { publicKey } = useWallet();
-
   const [referralLink, setReferralLink] = useState('');
 
   useEffect(() => {
@@ -17,23 +16,23 @@ const GenerateReferralLink = () => {
 
   return (
     <div className={styles.generateReferralLink_main_container}>
- 
         <div className={styles.generateReferralLink_container}>
-
           <input type="text" value={referralLink} readOnly className={styles.generateReferralLink_input} />
-
+          
+          {/* Disable button if the wallet is not connected (publicKey is null) */}
           <button
-          className={styles.generateReferralLink_button}
+            className={styles.generateReferralLink_button}
             onClick={() => {
-              navigator.clipboard.writeText(referralLink);
-              alert('Referral link copied to clipboard!');
+              if (publicKey) {
+                navigator.clipboard.writeText(referralLink);
+                alert('Referral link copied to clipboard!');
+              }
             }}
+            disabled={!publicKey} // Disable button if publicKey is not available
           >
             COPY REFERRAL LINK
           </button>
-
         </div>
-      
     </div>
   );
 };
