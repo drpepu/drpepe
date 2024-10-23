27efa231c0b3663621c3050b64e1c8e52c648b14
Victor Maragioglio <maragiogliov@protonmail.com>
2024-10-23 23:20:14 +0100

    Links and Button State fixs

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,14 +5,16 @@ import pepeheadshot from '../../Assets/DRPEPEVACCINEHEADSHOT.svg';
 import LanguageSwitcher from '../LanguageSwitcher/LanguageSwitcher';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 
 
 function Header() {
   const [isOpen, setIsOpen] = useState(false);
   const [activeLanguage, setActiveLanguage] = useState('english');
   const [activeLink, setActiveLink] = useState('home'); // State to track active link
-
   const sideNavRef = useRef(null);
+  const location = useLocation(); // Get the current route
+
 
   const toggleMenu = (link) => {
     setIsOpen(!isOpen);
@@ -24,6 +26,24 @@ function Header() {
   const changeLanguage = (lng) => {
     i18n.changeLanguage(lng);
     setActiveLanguage(lng);
+  };
+
+  const openTelegramLink = () => {
+    const mobileLink = 'tg://resolve?domain=drpepeai'; // Telegram mobile app link
+    const webLink = 'https://web.telegram.org/a/#-1002428485287'; // Telegram web link
+
+    const isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      window.location.href = mobileLink;
+    } else {
+      window.open(webLink, '_blank', 'noopener noreferrer');
+    }
+  };
+
+  const openXLink = () => {
+    const webLink = 'https://x.com/drpepeai'; // X web link
+    window.open(webLink, '_blank', 'noopener noreferrer');
   };
 
   const { t } = useTranslation();
@@ -54,23 +74,39 @@ function Header() {
 
   return (
     <>
-      <div className={styles.header_container}>
-      <a href="/"  className={`${styles.header_button_one} ${styles.header_button}`}>DRPEPE.AI</a>
-      <a href="https://docs.drpepe.ai/" target="_blank" rel="noreferrer" className={`${styles.header_button_four} ${styles.header_button}`}>DOCS</a>
-      <a href="/dapp" target="_blank"rel="noreferrer" className={`${styles.header_button_five} ${styles.header_button}`}>APP</a>
-      <a href="https://t.me/+yxh5qd2tKQU2ODIx" target="_blank"rel="noreferrer" className={`${styles.header_button_five} ${styles.header_button}`}>DRP</a>
 
-     
+      <div className={styles.header_container}>
+
+
+        <a href="https://docs.drpepe.ai/" target="_blank" rel="noreferrer" className={`${styles.header_button_four} ${styles.header_button}`}>
+          DOCS
+        </a>
+        <Link
+          to="/dapp"
+          className={`${styles.header_button_five} ${styles.header_button} ${
+            location.pathname === '/dapp' ? styles.activeButton : ''
+          }`}
+          >
+          ASK DR.PEPE
+        </Link>
+
+        {/* Button to open Telegram */}
+        <button onClick={openTelegramLink} className={`${styles.header_button_five} ${styles.header_button}`}>
+          DRP
+        </button>
 
         <LanguageSwitcher />
 
         <div className={styles.header_container_mobile}>
-        <Link className={styles.header_link} to="/">
-          <div className={styles.header_container_headshot_title}>
-            <img src={pepeheadshot} alt="dr pepe headshot" className={styles.mobile_pepe_headshot} />
-            <div className={styles.mobile_pepe_title}>DR.PEPE AI</div>
-          </div>
-        </Link>
+
+          <Link className={styles.header_link} to="/">
+            <div className={styles.header_container_headshot_title}>
+              <img src={pepeheadshot} alt="dr pepe headshot" className={styles.mobile_pepe_headshot} />
+              <div className={styles.mobile_pepe_title}>DR.PEPE AI</div>
+            </div>
+          </Link>
+
+       
 
           <div onClick={() => toggleMenu()}>
             <svg className={styles.mobile_pepe_hamburger} width="40" height="23" viewBox="0 0 40 23" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -79,6 +115,7 @@ function Header() {
               <rect y="18" width="40" height="5" rx="2.5" fill="white" />
             </svg>
           </div>
+
         </div>
 
         <div className={styles.header_sidenav} ref={sideNavRef}>
@@ -89,13 +126,32 @@ function Header() {
               <p>{t('side_navigation_title_one')}</p>
               <hr className={styles.hrClass} />
             </div>
-            <a href="/" className={`${styles.header_sideNavLink} ${activeLink === 'home' ? styles.active : ''}`} onClick={() => toggleMenu('/')}>Home</a>
 
-            <a href="https://docs.drpepe.ai/" target='_blank' rel="noreferrer" className={`${styles.header_sideNavLink} ${activeLink === 'docs' ? styles.active : ''}`} onClick={() => toggleMenu('docs')}>Docs</a>
+            <Link
+              to="/"
+              className={`${styles.header_sideNavLink} ${location.pathname === '/' ? styles.active : ''}`}
+              onClick={() => toggleMenu()}
+            >
+              Home
+            </Link>
 
-            <a href="/dapp" className={`${styles.header_sideNavLink} ${activeLink === 'app' ? styles.active : ''}`} onClick={() => toggleMenu('app')}>dApp</a>
+            <a href="https://docs.drpepe.ai/" target="_blank" rel="noreferrer" className={`${styles.header_sideNavLink} ${activeLink === 'docs' ? styles.active : ''}`} onClick={() => toggleMenu('docs')}>
+              Docs
+            </a>
 
-            <a href="https://t.me/+yxh5qd2tKQU2ODIx" className={`${styles.header_sideNavLink} ${activeLink === 'app' ? styles.active : ''}`} onClick={() => toggleMenu('app')}>DRP</a>
+            <Link
+              to="/dapp"
+              className={`${styles.header_sideNavLink} ${location.pathname === '/dapp' ? styles.active : ''}`}
+              onClick={() => toggleMenu('app')}
+            >
+              Ask Dr.Pepe
+            </Link>
+
+            <button onClick={openTelegramLink} className={`${styles.header_sideNavLink} ${activeLink === 'drp' ? styles.active : ''}`}>
+              DRP
+            </button>
+
+
 
           </div>
 
@@ -105,9 +161,15 @@ function Header() {
               <hr className={styles.hrClass} />
             </div>
             <div className={styles.header_sideNavLink_content}>
-              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'english' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('english'); }}>ENGLISH</button>
-              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'chinese' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('chinese'); }}>中文</button>
-              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'korean' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('korean'); }}>중국어</button>
+              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'english' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('english'); }}>
+                ENGLISH
+              </button>
+              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'chinese' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('chinese'); }}>
+                中文
+              </button>
+              <button className={`${styles.header_sideNavLink_small} ${activeLanguage === 'korean' ? styles.active : ''}`} onClick={() => { toggleMenu(); changeLanguage('korean'); }}>
+                한국어
+              </button>
             </div>
           </div>
 
@@ -117,12 +179,18 @@ function Header() {
               <hr className={styles.hrClass} />
             </div>
             <div className={styles.header_sideNavLink_content}>
-              <a href="https://x.com/drpepeai" className={`${styles.header_sideNavLink_small}`} onClick={() => toggleMenu()}>X</a>
-              <a href="https://t.me/+yxh5qd2tKQU2ODIx" className={`${styles.header_sideNavLink_small}`} onClick={() => toggleMenu()}>TELEGRAM</a>
+              <button onClick={openXLink} className={`${styles.header_sideNavLink_small}`}>
+                X
+              </button>
+              <button onClick={openTelegramLink} className={`${styles.header_sideNavLink_small}`}>
+                TELEGRAM
+              </button>
             </div>
           </div>
         </div>
       </div>
+
+
     </>
   );
 }
