985805ad13f4bae2cc5511e4ac94ec8ab0f32e3d
Victor Maragioglio <maragiogliov@protonmail.com>
2024-12-10 12:19:30 +0000

    Test New Confirm

--- src/pages/ReferralGraph/ReferralGraph.js
+++ src/pages/ReferralGraph/ReferralGraph.js
@@ -4,13 +4,7 @@ import Header from '../../components/Header/Header';
 import FooterSimple from '../../components/FooterSimple/FooterSimple';
 import ReferralGraphsHeader from '../../components/ReferralGraphsHeader/ReferralGraphsHeader';
 import ReferralLinksList from '../../components/ReferralLinkList/ReferralLinkList';
-import FirebaseDuplicateCollection from '../../components/FirebaseLab/FirebaseDuplicateCollection';
-import FirebaseDeleteReservedCharacters from '../../components/FirebaseLab/FirebaseDeleteReservedCharacters';
-import FirebaseSchemaTransform from '../../components/FirebaseLab/FirebaseSchemaTransform';
-import FirebaseSchemaCleanUp from '../../components/FirebaseLab/FirebaseSchemaCleanUp';
-import FirebaseUpdateDocumentIDs from '../../components/FirebaseLab/FirebaseUpdateDocumentId';
-import FirebaseAddLevel1AndPoints from '../../components/FirebaseLab/FirebaseAddLevel1AndPoints';
-import FirebaseAddLevel2Points from '../../components/FirebaseLab/FirebaseAddLevel2Points'
+
 
 
 
